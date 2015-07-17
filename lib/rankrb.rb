4415086e73b0b6fa966602e4b5e215b7f771671e
DaniG2k <byakugan.87@gmail.com>
2015-07-17 23:00:03 +0100

    Added bm25 method to Collection class

--- lib/rankrb.rb
+++ lib/rankrb.rb
@@ -2,7 +2,6 @@ require "rankrb/version"
 require "rankrb/query_cleaner"
 require "rankrb/document"
 require "rankrb/collection"
-require "rankrb/ranker"
 
 module Rankrb
   # Your code goes here...
--- lib/rankrb/collection.rb
+++ lib/rankrb/collection.rb
@@ -6,6 +6,10 @@ module Rankrb
       @query = params[:query]
     end
 
+    def each(&block)
+      self.each(&block)
+    end
+
     def containing_term(term)
       total = 0
       @docs.each do |doc|
@@ -25,5 +29,33 @@ module Rankrb
     def total_docs
       @docs.size
     end
+
+    def idf(term)
+      numerator = total_docs - containing_term(term) + 0.5
+      denominator = containing_term(term) + 0.5
+      Math.log(numerator / denominator)
+    end
+
+    def bm25(params={:k => 1.2, :b => 0.75, :delta => 1.0})
+      @k = params[:k]
+      @b = params[:b]
+      @delta = params[:delta]
+
+      @docs.each do |doc|
+        score = 0
+        dl = doc.length
+        query_terms = @query.split
+
+        query_terms.each do |term|
+          dtf = doc.term_freq(term)
+          numerator = dtf * (@k + 1)
+          denominator = dtf + @k * (1 - @b + @b * (doc.length / avg_dl))
+          score += idf(term) * (numerator/denominator) + @delta
+        end
+        doc.rank = score
+      end
+      @docs
+    end
+    
   end
 end
