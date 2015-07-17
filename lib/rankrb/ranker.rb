5f8dc176f37561a5cf3a87a6ca17680507de7f3d
DaniG2k <byakugan.87@gmail.com>
2015-07-17 21:31:08 +0100

    First commit

--- lib/rankrb/ranker.rb
+++ lib/rankrb/ranker.rb
@@ -1,10 +1,10 @@
 module Rankrb
-	class Ranker
-		attr_accessor :collection, :query
-		
-		def initialize(params={:collection => nil, :query => nil})
-			@collection = params[:collection]
-			@query = params[:query]
-		end
-	end
+  class Ranker
+    attr_accessor :collection, :query
+    
+    def initialize(params={:collection => nil, :query => nil})
+      @collection = params[:collection]
+      @query = params[:query]
+    end
+  end
 end
