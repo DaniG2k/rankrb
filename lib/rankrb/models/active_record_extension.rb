714df5f52c05ed13e8e54a858890a8d2961e30d1
DaniG2k <byakugan.87@gmail.com>
2015-07-25 17:05:18 +0100

    Latest commit

--- lib/rankrb/models/active_record_extension.rb
+++ lib/rankrb/models/active_record_extension.rb
@@ -3,14 +3,14 @@ module ActiveRecordExtension
 
   class_methods do
     def search(str)
-    	"searching"
+      "searching"
       # Run a find query that looks at the inverted index.
       # Then use Rankrb::Collection's bm25 method to rank the results.
     end
   end
 
   included do
-  	before_save :add_to_inverted_index
+    after_save :add_to_inverted_index
   end
 
   def add_to_inverted_index
--- lib/tasks/inverted_index.rake
+++ lib/tasks/inverted_index.rake
@@ -1,4 +1,10 @@
 namespace :db do
+  desc "Create inverted index table"
+  task :create_inverted_index => :environment do
+    `rails g migration RankrbIndex word:string:index unit_id:string`
+    `rake db:migrate`
+  end
+
   desc "Build the inverted index"
   task :build_inverted_index => :environment do
     puts "Executing inverted index rake task..."
