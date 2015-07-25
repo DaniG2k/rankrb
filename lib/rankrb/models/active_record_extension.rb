741606b5ec136a2399af5d1eea5e8a9172f4d647
DaniG2k <byakugan.87@gmail.com>
2015-07-25 14:25:30 +0100

    Intercepting before_save

--- lib/rankrb/models/active_record_extension.rb
+++ lib/rankrb/models/active_record_extension.rb
@@ -1,11 +1,19 @@
 module ActiveRecordExtension
   extend ActiveSupport::Concern
 
-  module ClassMethods
+  class_methods do
     def search(str)
+    	"searching"
       # Run a find query that looks at the inverted index.
       # Then use Rankrb::Collection's bm25 method to rank the results.
     end
+  end
+
+  included do
+  	before_save :add_to_inverted_index
+  end
+
+  def add_to_inverted_index
   end
 end
 # Include the extension 
--- lib/tasks/inverted_index.rake
+++ lib/tasks/inverted_index.rake
@@ -1,6 +1,6 @@
 namespace :db do
   desc "Build the inverted index"
-  task :build_inverted_index => :environemnt do
+  task :build_inverted_index do
     puts "Executing inverted index rake task..."
   end
 end
