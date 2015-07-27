997443e633fecb5f031d031d21836726cbdb23e7
Daniele Pestilli <dpestilli@funnelback.com>
2015-07-27 16:45:11 +0100

    Added Korean and Japanese tokenization mini-test

--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -6,4 +6,16 @@ describe Rankrb::Tokenizer do
     tokenizer = Rankrb::Tokenizer.new(str)
     expect(tokenizer.tokenize).to eq(%w(this is a body))
   end
+
+  it 'tokenizes a Japanese string' do
+    str = "これは、文書体です。"
+    tokenizer = Rankrb::Tokenizer.new(str)
+    expect(tokenizer.tokenize).to eq(["これは文書体です"])
+  end
+
+  it 'tokenizes a Korean string' do
+    str = "이것은 문서 체입니다."
+    tokenizer = Rankrb::Tokenizer.new(str)
+    expect(tokenizer.tokenize).to eq(['이것은', '문서', '체입니다'])
+  end
 end
