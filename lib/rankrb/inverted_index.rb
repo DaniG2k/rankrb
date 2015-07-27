07f277f7535df5b4e109777a38f4f03ac2eef307
Daniele Pestilli <dpestilli@funnelback.com>
2015-07-27 17:14:58 +0100

    Added InvertedIndex class

---
+++ lib/rankrb/inverted_index.rb
@@ -0,0 +1,16 @@
+module Rankrb
+  class InvertedIndex
+    def initialize(docid)
+      @docid = docid
+    end
+
+    def build(tokens)
+    end
+
+    def search(str)
+    end
+
+    def delete(str)
+    end
+  end
+end
---
+++ spec/inverted_index_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Rankrb::InvertedIndex do
+  it 'initializes taking a docID' do
+  #  expect(false).to eq(true)
+  end
+  it 'builds and inverted index' do
+  #  expect(false).to eq(true)
+  end
+  it 'searches the inverted index' do
+  #  expect(false).to eq(true)
+  end
+  it 'deletes the docIDs from the inverted index' do
+  #  expect(false).to eq(true)
+  end
+end
